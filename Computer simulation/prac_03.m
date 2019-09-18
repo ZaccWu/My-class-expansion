acfda9ffd82622a5e6312ccab1f0443360c9fe71
ZIYUE WU <37011471+Tinky2013@users.noreply.github.com>
2019-09-18 18:17:51 +0800

    Update prac_03.m

--- Computer simulation/prac_03.m
+++ Computer simulation/prac_03.m
@@ -10,3 +10,9 @@ plot(x,y1,'r',x,y2,'b')
 x=0.1:0.1:50;
 y=sin(x)./(x.^2);
 plot(x,y)
+
+% 3-10
+[x1,minf]=fminbnd('2*(sin(2*x))^2+2.5*x*(cos(x/2))^2',0,pi)
+[x2,maxf]=fminbnd('-(2*(sin(2*x))^2+2.5*x*(cos(x/2))^2)',0,pi)
+
+% 3-13

e4f225bf4ea3c41af4d345047ebda31d87e8f2cc
ZIYUE WU <37011471+Tinky2013@users.noreply.github.com>
2019-09-24 08:40:13 +0800

    Update Central limit theorem.R

--- Multivariate statistical analysis/Central limit theorem.R
+++ Multivariate statistical analysis/Central limit theorem.R
@@ -5,6 +5,7 @@ y2<-rep(0,1000)
 y3<-rep(0,1000)
 y4<-rep(0,1000)
 
+# CLT with different sample size
 for (i in 1:1000){
   y1[i]<-mean(runif(10,0,1))
   y2[i]<-mean(runif(100,0,1))
@@ -12,10 +13,25 @@ for (i in 1:1000){
   y4[i]<-mean(runif(10000,0,1))
 }
 bk<-seq(0.2,0.8,0.01)
-
 # plot
 par(mfrow=c(2,2))
 hist(y1,breaks=bk)
 hist(y2,breaks=bk)
 hist(y3,breaks=bk)
 hist(y4,breaks=bk)
+
+# CLT with different distributions
+for (i in 1:1000){
+  y1[i]<-mean(runif(100,0,1))
+  y2[i]<-mean(rnorm(100,0.5,1))
+  y3[i]<-mean(rnorm(100,0.5,2))
+  y4[i]<-mean(rbinom(100,1,0.5))
+}
+bk1<-seq(0,1,0.01)
+bk2<-seq(-1,2,0.01)
+
+par(mfrow=c(2,2))
+hist(y1,breaks=bk1)
+hist(y2,breaks=bk1)
+hist(y3,breaks=bk2)
+hist(y4,breaks=bk1)

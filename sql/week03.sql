e4094c19e2616a851e3b4623359ecf4090522ed2
ZIYUE WU <37011471+Tinky2013@users.noreply.github.com>
2019-09-28 09:18:43 +0800

    Update week03.sql

--- sql/week03.sql
+++ sql/week03.sql
@@ -5,13 +5,41 @@ select * from y_emails order by occurred_at
 select * from y_events order by occurred_at
 select * from y_users order by created_at
 
--- 处理时间字符并加入查询表中的时间列
-select * from y_users
+-- step1 数据预处理
+-- 处理时间字符、加入查询表中的时间列并将join之后的表格进行存储
+select * 
+into b_users  -- b开头的为备份文件
+from y_users
 left join time_pc
 on time_pc.date=substring(convert(varchar(10),y_users.created_at),0,11)
-select * from y_emails
+
+select *
+into b_emails
+from y_emails
 left join time_pc
 on time_pc.date=substring(convert(varchar(10),y_emails.occurred_at),0,11)
-select * from y_events
+
+select * 
+into b_events
+from y_events
 left join time_pc
 on time_pc.date=substring(convert(varchar(10),y_events.occurred_at),0,11)
+
+-- step2 数据探索
+-- 各个类别的活动总和
+select action as action_count, count(*) as times from b_emails group by action order by times desc
+select event_type as event_type_count, count(*) as times from b_events group by event_type order by times desc
+select event_name as event_name_count, count(*) as times from b_events group by event_name order by times desc
+select company_id as comid_count, count(*) as times from b_users group by company_id order by times desc
+-- 是否active和language的关系
+select language,
+count(case when state='active' then state else null end) as active,
+count(case when state='pending' then state else null end) as pending
+from y_users group by language
+order by active desc,pending desc
+-- 是否active和company_id的关系
+select company_id,
+count(case when state='active' then state else null end) as active,
+count(case when state='pending' then state else null end) as pending
+from y_users group by company_id
+order by active desc,pending desc

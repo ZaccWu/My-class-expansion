701d44f1209e2d27e0e80d6154990432495798e7
ZIYUE WU <37011471+Tinky2013@users.noreply.github.com>
2019-09-28 10:57:47 +0800

    Update week03.sql

--- sql/week03.sql
+++ sql/week03.sql
@@ -50,6 +50,8 @@ from b_events group by period_id order by period_id
 select period_id,
 count(distinct user_id) as num_users
 from b_emails group by period_id order by period_id
+
+-- 以下输出结果将会进行曲线图的绘制
 -- 分别统计每个period_id中注册的active和panding用户
 select period_id, time_id,
 count(case when state='active' then state else null end) as active_users,
@@ -60,3 +62,27 @@ select date, period_id, time_id,
 count(case when state='active' then state else null end) as active_users,
 count(case when state='pending' then state else null end) as pending_users
 from b_users group by date,time_id,period_id order by date
+-- emails中各类action的数量变化（每周）
+select period_id, time_id,
+count(case when action='sent_weekly_digest' then action else null end) as sent_weekly_digest,
+count(case when action='email_open' then action else null end) as email_open,
+count(case when action='email_clickthrough' then action else null end) as email_clickthrough,
+count(case when action='sent_reengagement_email' then action else null end) as sent_reengagement_email
+from b_emails group by time_id,period_id order by time_id
+-- event_type中的数量变化（每周）
+select period_id, time_id,
+count(case when event_type='engagement' then event_type else null end) as engagement,
+count(case when event_type='signup_flow' then event_type else null end) as signup_flow
+from b_events group by time_id,period_id order by time_id
+-- event_name的数量变化（每周）
+select period_id, time_id,
+count(case when event_name='home_page' then event_name else null end) as home_page,
+count(case when event_name='like_message' then event_name else null end) as like_message,
+count(case when event_name='view_inbox' then event_name else null end) as view_inbox,
+count(case when event_name='login' then event_name else null end) as login,
+count(case when event_name='send_message' then event_name else null end) as send_message,
+count(case when event_name='search_autocomplete' then event_name else null end) as search_autocomplete,
+count(case when event_name='search_run' then event_name else null end) as search_run,
+count(case when event_name='create_user' then event_name else null end) as create_user,
+count(case when event_name='enter_email' then event_name else null end) as enter_email
+from b_events group by time_id,period_id order by time_id
